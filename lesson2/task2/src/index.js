c13daacfcb0cb1c2c26c6c0fe27fc54baf85d4f0
kate <kata3140@gmail.com>
2024-12-03 15:00:22 +0200

    new

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -1,6 +1,18 @@
 import './index.scss';
+import store, { increment } from './store';
 
 const resultElem = document.querySelector('.counter');
-const incrementBtn = document.querySelector('[data]');
-const resetBtn = document.querySelector('[data]');
-const decrementBtn = document.querySelector('[data]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action ="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onIncrement = () => {
+    store.dispatch(increment());
+}
+
+incrementBtn.addEventListener('click', onIncrement);
+
+store.subsribe(() => {
+    store.getState();
+    resultElem.textContent = result;
+})
