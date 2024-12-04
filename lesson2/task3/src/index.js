224b0b0d9072998fc6c08ab2dc9e3b7dba02601a
kate <kata3140@gmail.com>
2024-12-04 17:58:14 +0200

    new

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
-import store, { increment, decrement, reset } from './store';
+import store, { increment, decrement, reset, addUser, deleteUser } from './store';
 
 const resultElem = document.querySelector('.counter__result');
 const incrementBtn = document.querySelector('[data-action="increment"]');
@@ -7,27 +7,29 @@ const resetBtn = document.querySelector('[data-action ="reset"]');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 
 const onIncrement = () => {
-    store.dispatch(increment());
+    store.dispatch(addUser());
+    console.log(store.getState());
 }
 
 const onDecrement = () => {
-    store.dispatch(decrement());
+    store.dispatch(deleteUser());
 }
 
-const onReset = () => {
-    store.dispatch(reset());
-}
+// const onReset = () => {
+//     store.dispatch(reset());
+// }
+
 incrementBtn.addEventListener('click', onIncrement);
 decrementBtn.addEventListener('click', onDecrement);
-resetBtn.addEventListener('click', onReset);
+// resetBtn.addEventListener('click', onReset);
 
 store.subscribe(() => {
-    const state = store.getState();
-    const currentValue = state.history.reduce((acc, value) => acc + value, 0);
-    const historyString = state.history.join(' ');
+    // const state = store.getState();
+    // const currentValue = state.history.reduce((acc, value) => acc + value, 0);
+    // const historyString = state.history.join(' ');
 
-    resultElem.textContent = state.history.length === 0
-        ? ''
-        : `${historyString} = ${currentValue}`;
-    // console.log(store.getState());
+    // resultElem.textContent = state.history.length === 0
+    //     ? ''
+    //     : `${historyString} = ${currentValue}`;
+    console.log(store.getState());
 })
--- lesson2/task3/src/store.js
+++ lesson2/task3/src/store.js
@@ -1,53 +1,91 @@
 import { createStore } from 'redux';
 
-const INCREMENT = 'COUNTER/INCREMENT';
-const DECREMENT = 'COUNTER/DECREMENT';
-const RESET = 'COUNTER/RESET';
+// const INCREMENT = 'COUNTER/INCREMENT';
+// const DECREMENT = 'COUNTER/DECREMENT';
+// const RESET = 'COUNTER/RESET';
 
-export const increment = () => {
+const ADD = 'USER/ADD';
+const DELETE = 'USER/DELETE';
+
+
+export const addUser = () => {
     return {
-        type: INCREMENT,
+        type: ADD,
     }
 }
-export const decrement = () => {
+
+export const deleteUser = () => {
     return {
-        type: DECREMENT,
+        type: DELETE,
     }
 }
-export const reset = () => {
-    return {
-        type: RESET,
-    }
-}
+
+// export const increment = () => {
+//     return {
+//         type: INCREMENT,
+//     }
+// }
+// export const decrement = () => {
+//     return {
+//         type: DECREMENT,
+//     }
+// }
+// export const reset = () => {
+//     return {
+//         type: RESET,
+//     }
+// }
+
+// export const initialState = {
+//     history: [],
+// }
 
 export const initialState = {
-    history: [],
+    usersList: [],
 }
 
-export const counterReducer = (state = initialState, action) => {
+export const userReducer = (state = usersList, action) => {
     switch (action.type) {
-        case INCREMENT:
+        case ADD:
             return {
                 ...state,
-                value: state.value + 1,
-                history: state.history.concat(1)
+                usersList: [...state.usersList, 1]
             };
-        case DECREMENT:
+        case DELETE:
             return {
                 ...state,
-                value: state.value - 1,
-                history: state.history.concat(-1)
-            };
-        case RESET:
-            return {
-                value: 0,
-                history: [],
+                usersList: state.usersList.slice(-1),
             }
         default:
             return state;
     }
 }
 
-export const store = createStore(counterReducer);
+// export const counterReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case INCREMENT:
+//             return {
+//                 ...state,
+//                 value: state.value + 1,
+//                 history: state.history.concat(1)
+//             };
+//         case DECREMENT:
+//             return {
+//                 ...state,
+//                 value: state.value - 1,
+//                 history: state.history.concat(-1)
+//             };
+//         case RESET:
+//             return {
+//                 value: 0,
+//                 history: [],
+//             }
+//         default:
+//             return state;
+//     }
+// }
+
+// export const store = createStore(counterReducer);
+export const store = createStore(userReducer);
 
 export default store;
