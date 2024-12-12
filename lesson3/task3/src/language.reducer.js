484b00eea6eb081f77fdf77ec160d39a8395f2bb
kate <kata3140@gmail.com>
2024-12-12 21:24:20 +0200

    new

--- lesson3/task3/src/language.reducer.js
+++ lesson3/task3/src/language.reducer.js
@@ -1,16 +1,11 @@
 import { SET_LANGUAGE } from "./language.actions";
 
-const initialState = {
-    language: 'en'
-};
 
-export const languageReducer = (state = initialState, action) => {
+
+export const languageReducer = (state = 'en', action) => {
     switch (action.type) {
         case SET_LANGUAGE:
-            return {
-                ...state,
-                language: action.payload.lang
-            }
+            return action.payload.lang;
         default:
             return state;
     }
