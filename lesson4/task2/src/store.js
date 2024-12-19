0d13861eda89b04dd37cd03a50ac01619eb1980a
kate <kata3140@gmail.com>
2024-12-19 15:56:35 +0200

    new

--- lesson4/task2/src/store.js
+++ lesson4/task2/src/store.js
@@ -9,8 +9,8 @@ const reducer = combineReducers({
 })
 
 const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    reducer
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 );
 
 export default store;
