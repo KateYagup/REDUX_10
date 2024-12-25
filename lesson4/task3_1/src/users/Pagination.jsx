d445f8661e7685b35359afe22594872d9da6a363
kate <kata3140@gmail.com>
2024-12-25 18:54:07 +0200

    new

--- lesson4/task3_1/src/users/Pagination.jsx
+++ lesson4/task3_1/src/users/Pagination.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, goNext, goPrev }) => {
-    const isPrevPageAvailable = currentPage === 0;
+    const isPrevPageAvailable = currentPage > 0;
 
-    const isNextPageAvailable = currentPage === totalPages - 1;
+    const isNextPageAvailable = currentPage < totalPages - 1;
 
     return (
         < div className="pagination" >
             <button
                 className="btn"
                 onClick={goPrev}
-                disabled={isPrevPageAvailable}
+                disabled={!isPrevPageAvailable}
             >
-                {!isPrevPageAvailable && '←'}
+                {isPrevPageAvailable && '←'}
             </button>
             < span className="pagination__page" >
                 {currentPage + 1}
@@ -20,9 +20,9 @@ const Pagination = ({ currentPage, totalPages, goNext, goPrev }) => {
             <button
                 className="btn"
                 onClick={goNext}
-                disabled={isNextPageAvailable}
+                disabled={!isNextPageAvailable}
             >
-                {!isNextPageAvailable && '→'}
+                {isNextPageAvailable && '→'}
             </button>
         </div >
     )
